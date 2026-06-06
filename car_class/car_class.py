1c28b1c88c78076a7fe03b1413459d877a6a5479
ivanmula-1 <ivanmula74@gmail.com>
2026-06-06 22:36:27 +0800

    Add accelerate method to car_class

--- car_class/car_class.py
+++ car_class/car_class.py
@@ -4,3 +4,9 @@ class Car:
         self.__year_model = year_model
         self.__make = make
         self.__speed = 0
+
+def accelerate(self):
+        """Add 5 to the current speed."""
+        self.__speed += 5
+
+
