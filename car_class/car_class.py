543ddcbb38794e36a0218d86d4a2688e22f2a6c1
ivanmula-1 <ivanmula74@gmail.com>
2026-06-06 22:37:27 +0800

    Implement brake method in car_class
    Add brake method to reduce speed safely.

--- car_class/car_class.py
+++ car_class/car_class.py
@@ -9,4 +9,9 @@ def accelerate(self):
         """Add 5 to the current speed."""
         self.__speed += 5
 
-
+def brake(self):
+        """Subtract 5 from the current 
+        if self.__speed >= 5:
+            self.__speed -= 5
+        else:
+            self.__speed = 0
