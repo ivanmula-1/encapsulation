296e8d27980070c90a96d7642a49a4e04a5b6325
ivanmula-1 <ivanmula74@gmail.com>
2026-06-06 23:15:49 +0800

    Create class_pet.py

---
+++ class_pet/class_pet.py
@@ -0,0 +1,6 @@
+class Pet:
+    def __init__(self):
+        """Initialize private attributes with empty/default values."""
+        self.__name = ""
+        self.__animal_type = ""
+        self.__age = 0
