ce8463a35b8d9e710495aa2117298354bbe8d6d4
ivanmula-1 <ivanmula74@gmail.com>
2026-06-06 22:34:44 +0800

    Create Car class with initialization attributes

---
+++ car_class/car_class.py
@@ -0,0 +1,6 @@
+class Car:
+    def __init__(self, year_model, make):
+        """Initialize the car's attributes."""
+        self.__year_model = year_model
+        self.__make = make
+        self.__speed = 0
